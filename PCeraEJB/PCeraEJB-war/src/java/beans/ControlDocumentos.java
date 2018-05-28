466441b184e3690ee1554a05bee54a3b0c56cff1
Juan Antonio <Juan Antonio@LAPTOP-MEO5DRKE>
2018-05-28 21:26:33 +0200

    Añado manualmente las entidades JPA

--- PCeraEJB/PCeraEJB-war/src/java/beans/ControlDocumentos.java
+++ PCeraEJB/PCeraEJB-war/src/java/beans/ControlDocumentos.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package es.uma.informatica.sii.jsf.autenticacion;
+package beans;
 
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
--- PCeraEJB/PCeraEJB-war/src/java/beans/Login.java
+++ PCeraEJB/PCeraEJB-war/src/java/beans/Login.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package es.uma.informatica.sii.jsf.autenticacion;
+package beans;
 
 import es.uma.informatica.sii.jsf.autenticacion.modelo.Usuario;
 
